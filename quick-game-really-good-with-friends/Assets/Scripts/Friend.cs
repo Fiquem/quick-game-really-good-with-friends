c770fb09f42a24b0540ee86c76a4b7dd5338db1b
KillianMcCabe <killianparker93@gmail.com>
2016-11-16 20:25:48 +0000

    Added speech bubble

--- quick-game-really-good-with-friends/Assets/Scripts/Friend.cs
+++ quick-game-really-good-with-friends/Assets/Scripts/Friend.cs
@@ -15,33 +15,41 @@ public class Friend : MonoBehaviour {
     string selectedText = "";
     bool talking = false;
 
+    GameObject speechBubble;
+
     // Use this for initialization
     void Start () {
-	
-	}
+        speechBubble = Instantiate(Resources.Load("SpeechBubble-Small"), speechBubbleAnchor.transform, false) as GameObject;
+        speechBubble.SetActive(false);
+    }
 	
 	// Update is called once per frame
 	void Update () {
 
     }
 
-    void OnGUI()
-    {
-        if (talking)
-        {
-            Vector3 getPixelPos = Camera.main.WorldToScreenPoint(speechBubbleAnchor.transform.position);
-            getPixelPos.y = Screen.height - getPixelPos.y;
+    //void OnGUI()
+    //{
+    //    if (talking)
+    //    {
+    //        Vector3 getPixelPos = Camera.main.WorldToScreenPoint(speechBubbleAnchor.transform.position);
+    //        getPixelPos.y = Screen.height - getPixelPos.y;
             
-            GUI.Label(new Rect(getPixelPos.x, getPixelPos.y, 200f, 100f), selectedText);
-            //print(selectedText);
-        }
-    }
+    //        GUI.Label(new Rect(getPixelPos.x, getPixelPos.y, 200f, 100f), selectedText);
+    //        //print(selectedText);
+    //    }
+    //}
 
     public void talk()
     {
+        
         if (speech.Length > 0) {
             talking = true;
+            speechBubble.SetActive(true);
+
             selectedText = speech[speechIndex];
+            speechBubble.GetComponentInChildren<TextMesh>().text = selectedText;
+
             speechIndex++;
             if (speechIndex >= speech.Length)
             {
