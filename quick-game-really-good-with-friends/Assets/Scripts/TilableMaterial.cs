ddb003543662a37fa992578c16f825ec707f7028
KillianMcCabe <dlab@DLabs-MacBook-Pro.local>
2016-12-02 16:24:12 +0000

    Added new tileable shader with tileable dirt example

---
+++ quick-game-really-good-with-friends/Assets/Scripts/TilableMaterial.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+
+[ExecuteInEditMode]
+public class TilableMaterial : MonoBehaviour {
+
+	void Awake() {
+		GetComponent<MeshRenderer> ().sharedMaterial.mainTextureScale = new Vector2 (transform.localScale.x, transform.localScale.y);
+		GetComponent<MeshRenderer> ().sharedMaterial.mainTextureOffset = new Vector2 (transform.localScale.x/transform.localScale.y, transform.localScale.y/transform.localScale.y);
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	// Update is called once per frame
+	void Update () {
+		GetComponent<MeshRenderer> ().sharedMaterial.mainTextureScale = new Vector2 (transform.localScale.x, transform.localScale.y);
+		GetComponent<MeshRenderer> ().sharedMaterial.mainTextureOffset = new Vector2 (transform.position.x + transform.localScale.x/2, transform.position.y + transform.localScale.y/2);
+	}
+
+
+}
